957d9827fa268833bd179a17db3c65ed0b11a0c6
Hzz <xiaofei00152@gmail.com>
2021-09-29 14:21:15 +0000

    update jsp-v3.0/src/com/web/loginSvl.java.

--- jsp-v3.0/src/com/web/loginSvl.java
+++ jsp-v3.0/src/com/web/loginSvl.java
@@ -10,8 +10,29 @@ import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+/*
+    此处使用传统注册
+    <!--    登录servlet传统注册-->
+    <servlet>
+        <servlet-name>loginSvl</servlet-name>
+        <servlet-class>com.web.loginSvl</servlet-class>
+    </servlet>
+    <servlet-mapping>
+        <servlet-name>loginSvl</servlet-name>
+        <url-pattern>/login</url-pattern>
+    </servlet-mapping>
 
+    用户登录验证
 
+    请求方式：post
+    请求地址：/login
+    请求参数：
+        account 账户
+        pwd 密码
+    返回格式：text/html
+    返回参数：<script>alert('账号或密码不正确！');location='login.jsp';</script>
+*/
+//@WebServlet(name = "loginSvl", urlPatterns = "/login")
 public class loginSvl extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
