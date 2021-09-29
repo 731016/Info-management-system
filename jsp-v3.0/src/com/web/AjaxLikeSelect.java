95b0e12466fe5e82e61cd149194f017187232e69
Hzz <xiaofei00152@gmail.com>
2021-09-29 13:40:56 +0000

    update jsp-v3.0/src/com/web/AjaxLikeSelect.java.

--- jsp-v3.0/src/com/web/AjaxLikeSelect.java
+++ jsp-v3.0/src/com/web/AjaxLikeSelect.java
@@ -12,14 +12,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
 /*
-    处理模糊查询请求
-    data:{'acc':username}
-
-    接收字符串acc
-    返回：被序列化的对象，json格式的字符串
-*/
+    请求方式：post
+    请求地址："/ajaxAdd"
+    
+    请求参数：{'acc':username}
+    参数：acc - 请求获取的名称
+         username - 用户名
+         格式：'admin'
+    
+    返回格式：json字符串
+    返回参数：List<Student>序列化后的json字符串
+    格式：{'account':'admin','pwd':'123','sex':'男','educ':'本科','happy':'玩游戏,学习'}
+ */
 @WebServlet(name = "AjaxLikeSelect",urlPatterns = "/ajaxSelect")
 public class AjaxLikeSelect extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
