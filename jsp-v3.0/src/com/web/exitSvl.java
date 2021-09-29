3884aa77c33d6c898bd257cb9bb0e661728546e5
Hzz <xiaofei00152@gmail.com>
2021-09-29 14:13:33 +0000

    update jsp-v3.0/src/com/web/exitSvl.java.

--- jsp-v3.0/src/com/web/exitSvl.java
+++ jsp-v3.0/src/com/web/exitSvl.java
@@ -7,7 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+/*
+    退出登录
+    清除session信息
 
+    请求方式：get
+    请求地址：/exit
+    请求参数：无
+
+    重定向：login.jsp
+*/
 @WebServlet(name = "exitSvl",urlPatterns = "/exit")
 public class exitSvl extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
